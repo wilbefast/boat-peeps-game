b37efae00e4851a06afa8c582ff5c77eb6df34d5
William Dyce <wilbefast@gmail.com>
2015-01-20 21:46:00 +0100

    new android deploy

--- android_deploy.sh
+++ android_deploy.sh
@@ -4,8 +4,7 @@ LOVE_ANDROID=../love-android-sdl2
 
 cd src/
 zip -r game.love * -x *.git*
-
+cd ../
 mv game.love ${LOVE_ANDROID}/assets
 cd $LOVE_ANDROID
-ant debug
-adb install -r ${LOVE_ANDROID}/bin/love_android_sdl2-debug.apk
+ant debug install
